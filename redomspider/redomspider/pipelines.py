542bc10540839320528d2935c3507ae42e5e67fd
DmitryKartashov <windbaria1000@gmail.com>
2021-03-19 21:23:23 +0700

    Сделал PIPELINE

--- redomspider/redomspider/pipelines.py
+++ redomspider/redomspider/pipelines.py
@@ -23,14 +23,51 @@ class RedomspiderPipeline:
 				password = es.DB_PASSWORD,
 				host = es.DB_HOST,
 				port = es.DB_PORT)
+		self.cur = self.con.cursor()
 
 	def close_spider(self, spider):
 		self.con.close()
 
 	def process_item(self, item, spider):
-		sql_insert_template = '''
-			insert into event
-			(title,date_event,time_event,location,description)
-			values('{}',{},{},{},'{}')
+		'''(title,date_event,time_event,location,description)'''
+		sql_insert_event_template = '''
+			WITH ev_key AS
+				(INSERT INTO event(title,date_event,time_event,location,description)
+							VALUES('{title}','{date_event}','{time_event}',{location},'{description}')
+							RETURNING id),
+				cat_key AS
+				(SELECT id FROM categories
+							WHERE category = '{category}'),
+				_ AS
+				(INSERT INTO category_event(category_id,event_id)
+						SELECT cat_key.id, ev_key.id
+						FROM cat_key,ev_key RETURNING id),
+				med_key AS
+				(SELECT id FROM media_type WHERE type = 'image')
+				INSERT INTO event_media(url,type_id,event_id)
+					SELECT '{url}',med_key.id,ev_key.id
+					FROM med_key, ev_key;
 		'''
+
+"""
+					{'ev_type':ev_type,
+						'ev_date':ev_date,
+						'ev_time':ev_time,
+						'ev_place':ev_place,
+						'ev_name':ev_name,
+						'ev_description':ev_description,
+						'ev_image_path':ev_image_abs_path}
+"""
+
+
+		sql_insert_event = sql_insert_event_template.format(title = item['ev_name'],
+															date_event = item['ev_date'],
+															time_event = item['ev_time'],
+															location = {'loc_name':item['place']},
+															description = item['ev_description'],
+															category = item['ev_type'],
+															url = item['ev_image_path'])
+
+		self.cur.execute(sql_insert_event)
+		self.cur.commit()
 		return item
